c6fbbfac5ba8da325f323c9a8856f06d9dac7704
minh12321 <caominhh725@gmail.com>
2025-04-10 13:59:34 +0700

    helo ửa

--- src/main/java/com/bongda/SecurityConf.java
+++ src/main/java/com/bongda/SecurityConf.java
@@ -5,14 +5,12 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@EnableWebSecurity
+
 @Configuration
 public class SecurityConf {
 
