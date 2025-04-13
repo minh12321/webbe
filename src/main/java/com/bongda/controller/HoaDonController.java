08d628d6239009072253540998a872528358774a
minh12321 <caominhh725@gmail.com>
2025-04-13 13:37:36 +0700

    bủ bủbủ

--- src/main/java/com/bongda/controller/HoaDonController.java
+++ src/main/java/com/bongda/controller/HoaDonController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bongda.model.HoaDon;
 import com.bongda.service.HoaDonService;
 
+@CrossOrigin(origins = "https://ab-mocha.vercel.app")
 @RestController
 @RequestMapping("/shopbongda/api/hoadon")
 public class HoaDonController {
